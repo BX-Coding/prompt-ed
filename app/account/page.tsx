dc764e1fba851141771e164e48f83bba6e2a8db5
Ava Wandersleben <78870469+Snoopy219@users.noreply.github.com>
2024-05-14 16:08:28 -0400

    delete console logs

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -42,7 +42,6 @@ export default function Home() {
   });
 
   if (user === "0") {
-    console.log("inside")
     return (
       <p>Access Denied</p>
     );
--- app/landing-page/page.tsx
+++ app/landing-page/page.tsx
@@ -21,7 +21,6 @@ export default function Home() {
   }
   CheckLogin();
   if (user === "0") {
-    console.log("inside")
     return (
       <p>Access Denied</p>
     );
