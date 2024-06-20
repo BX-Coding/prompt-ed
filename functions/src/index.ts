29c12ae8270060fef39673d025d2f56d132e39b7
Leighton Miguel <leim56@hawaii.rr.com>
2024-06-19 16:25:17 -1000

    Image and text moderation helper functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import OpenAI from "openai";
-import {defineString} from "firebase-functions/params";
+import { defineString } from "firebase-functions/params";
+import * as functions from "firebase-functions";
 const openaiKey = defineString("OPENAI_API_KEY");
+const axios = require("axios");
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -18,14 +20,61 @@ const dalleRequest = async (openai: OpenAI, prompt: string) => {
 };
 
 export const generateImage = onRequest(
-  {cors: true},
-  async (request: { body: { data: { prompt: any; }; }; }, response: { send: (arg0: any) => void; }) => {
-    const openai = new OpenAI({apiKey: openaiKey.value()});
+  { cors: true },
+  async (
+    request: { body: { data: { prompt: any } } },
+    response: { send: (arg0: any) => void }
+  ) => {
+    const openai = new OpenAI({ apiKey: openaiKey.value() });
 
     const prompt = request.body.data.prompt;
     logger.info(
       `Recieved ${request.body.data} Requesting dalle with prompt ${prompt}`,
-      {structuredData: true});
+      { structuredData: true }
+    );
     const dalleResponse = await dalleRequest(openai, prompt);
     response.send(dalleResponse);
-  });
+  }
+);
+
+const apiKey = functions.config().textmoderation.apikey;
+
+export const moderateText = async (text: string) => {
+  const endpoint =
+    "https://4s77vb8f38.execute-api.us-east-2.amazonaws.com/Deploy/text-moderation";
+  try {
+    const response = await axios.post(
+      endpoint,
+      { text: text },
+      {
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error moderating text :", error);
+  }
+};
+
+export const moderateImage = async (imageUrl: string) => {
+  const endpoint =
+    "https://4s77vb8f38.execute-api.us-east-2.amazonaws.com/Deploy/image-moderation";
+  try {
+    const response = await axios.post(
+      endpoint,
+      { image_url: imageUrl },
+      {
+        headers: {
+          "x-api-key": apiKey,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error moderating image :", error);
+  }
+};
