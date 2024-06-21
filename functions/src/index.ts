6b4204c734418d48e5a065a8c1913c1dc1dcd9ce
Leighton Miguel <leim56@hawaii.rr.com>
2024-06-20 16:58:11 -1000

    Refactored helper image function to interface

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,28 +1,11 @@
 import fetch from "node-fetch";
-import { createProdia } from "prodia";
 import { defineSecret } from "firebase-functions/params";
 import { onRequest } from "firebase-functions/v2/https";
 
+import { imageFunctions } from "./imageFunctions";
+
+// Define cloud secrets to be used
 const prodiaKey = defineSecret("prodia-key");
-
-const prodiaRequest = async (prompt: string, key: string): Promise<string> => {
-  const prodia = createProdia({
-    apiKey: key,
-  });
-
-  const job = await prodia.generate({
-    prompt: prompt,
-    model: "v1-5-pruned-emaonly.safetensors [d7049739]",
-  });
-
-  const { imageUrl, status } = await prodia.wait(job);
-
-  if (status === "succeeded") {
-    return imageUrl;
-  } else {
-    return "";
-  }
-};
 
 exports.generateImage = onRequest(
   { secrets: [prodiaKey] },
@@ -35,7 +18,10 @@ exports.generateImage = onRequest(
     }
 
     try {
-      const prodiaResponse = await prodiaRequest(prompt, prodiaKey.value());
+      const prodiaResponse = await imageFunctions.prodiaRequest(
+        prompt,
+        prodiaKey.value()
+      );
 
       if (prodiaResponse === "") {
         response.status(500).send("Failed to generate image");
