198f30bac7916d9a69d8679ed4e1f14c4f986d5c
Benjamin Montgomery <benjamin8yrs@gmail.com>
2024-06-25 12:03:28 -0400

    Homepage uses the hide/show button built into TabbedSidebar now

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -12,24 +12,14 @@ export default function Home() {
 
   const auth = getAuth();
   const [logIn, setLogIn] = useState(false);
-    onAuthStateChanged(auth, (user) => {
-      if (user && !logIn) {
-        setLogIn(true);
-      }
-      if (!user) {
-        router.push("/login");
-      }
-    });
-
-  const [showSidebar, setShowSidebar] = useState(true);
-
-  const toggleShowSidebar = () => {
-    if (showSidebar) {
-
+  onAuthStateChanged(auth, (user) => {
+    if (user && !logIn) {
+      setLogIn(true);
     }
-
-    setShowSidebar(!showSidebar);
-  }
+    if (!user) {
+      router.push("/login");
+    }
+  });
 
   if (!logIn) {
     return (
@@ -38,46 +28,46 @@ export default function Home() {
   } else {
     return (
       <div className="flex flex-col h-screen">
-        <NavBar navLocation="home"/>
+        <NavBar navLocation="home" />
         <div className="flex flex-row">
-          <div className={"bg-primary-foreground flex flex-row h-[calc(100vh-90px)] max-xl:absolute max-xl:top-[90px] max-xl:left-0 " + (showSidebar ? " max-xl:w-[calc(100%-20px)] xl:w-[600px] " : " w-5 ")}>
-            <div className={(showSidebar ? "" : "hidden")}>
-              <TabbedSidebar defaultValue="Lesson 1" tabs={[
-                {name: "Lesson 1", content:
-                  <p className="text-xl font-bold">
-                    Welcome to Neural Kingdom
-                  </p>
-                },
-                {name: "Lesson 2", content:
-                  <p className="text-xl font-bold">
-                    Welcome to Neural Kingdom
-                  </p>
-                },
-                {name: "Lesson 3", content:
-                  <p className="text-xl font-bold">
-                    Welcome to Neural Kingdom
-                  </p>
-                },
-                {name: "Lesson 4", content:
-                  <p className="text-xl font-bold">
-                    Welcome to Neural Kingdom
-                  </p>
-                },
-                {name: "Lesson 5", content:
-                  <p className="text-xl font-bold">
-                    Welcome to Neural Kingdom
-                  </p>
-                },
-              ]} />
-            </div>
-          </div>
-          <div>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
+          <TabbedSidebar defaultValue="Lesson 1" tabs={[
+            {
+              name: "Lesson 1", content:
+                <p className="text-xl font-bold">
+                  Welcome to Neural Kingdom
+                </p>
+            },
+            {
+              name: "Lesson 2", content:
+                <p className="text-xl font-bold">
+                  Welcome to Neural Kingdom
+                </p>
+            },
+            {
+              name: "Lesson 3", content:
+                <p className="text-xl font-bold">
+                  Welcome to Neural Kingdom
+                </p>
+            },
+            {
+              name: "Lesson 4", content:
+                <p className="text-xl font-bold">
+                  Welcome to Neural Kingdom
+                </p>
+            },
+            {
+              name: "Lesson 5", content:
+                <p className="text-xl font-bold">
+                  Welcome to Neural Kingdom
+                </p>
+            },
+          ]} />
         </div>
-        <Button size="sm" variant="accent" className={"absolute top-[200px] rounded-full " + (showSidebar ? " max-xl:left-[calc(100%-30px)] xl:left-[590px] " : " left-[10px] ")} onClick={toggleShowSidebar}>&gt;</Button>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
     );
   }
