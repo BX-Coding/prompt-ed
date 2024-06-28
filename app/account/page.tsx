62f6e98c218d60d28c7c9abd3c5197a072584334
Benjamin Montgomery <benjamin8yrs@gmail.com>
2024-06-28 16:24:34 -0400

    Style accounts page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,26 +1,38 @@
 "use client"
 
 import { NavBar } from "@/components/navbar"
-import { getAuth, onAuthStateChanged} from "firebase/auth";
-import { useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useCallback, useEffect, useState } from "react";
 import React from "react";
 import { UpdatePassword } from "@/components/update-password-form";
 import { AlertDelete } from "@/components/alert-delete";
 import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { RightArrowIcon } from "@/components/icons/prompt-ed-icons";
+import { Toaster } from "@/components/ui/toaster";
 export default function Home() {
 
   const [email, setEmail] = useState('');
 
   const auth = getAuth();
   const [logIn, setLogIn] = useState(false);
-    onAuthStateChanged(auth, (user) => {
-      if (user && user.email !== null) {
-        setEmail(user.email);
-      }
-      if (user && !logIn) {
-        setLogIn(true);
-      }
-    });
+  const [updatePasswordShow, setUpdatePasswordShow] = useState(false);
+
+  const showUpdatePassword = () => {
+    setUpdatePasswordShow(true);
+  };
+  const hideUpdatePassword = () => {
+    setUpdatePasswordShow(false);
+  }
+
+  useEffect(() => {onAuthStateChanged(auth, (user) => {
+    if (user && user.email !== null) {
+      setEmail(user.email);
+    }
+    if (user && !logIn) {
+      setLogIn(true);
+    }
+  })}, [auth, logIn]);
 
   if (!logIn) {
     return (
@@ -28,19 +40,14 @@ export default function Home() {
     );
   } else {
     return (
-        <div className="flex flex-col h-screen">
-          <NavBar navLocation="account"/>
-          <Card className="mx-12 my-6 p-12">
-            <div className="flex flex-col items-start gap-4">
-              <h1 className="text-title-xl text-black text-center font-bold">Account Information</h1>
-              <h3 className="text-xl text-center font-bold text-muted">Email: {email}</h3>
-              <h3 className="text-xl text-center font-bold text-muted">Update Password:</h3>
-              <UpdatePassword />
-              <div className="mt-20">
-                <AlertDelete />
-              </div>
-            </div>
-          </Card>
+      <div className="flex flex-col h-screen">
+        <NavBar navLocation="account" />
+        <div className="rounded-xl border bg-card-solid mx-12 my-6">
+          <Button variant="ghost" size="menu" className="flex w-full rounded-none">Account Details<div className="flex-1" /><RightArrowIcon width={16} height={16} /></Button>
+          <Button onClick={showUpdatePassword} variant="ghost" size="menu" className="flex w-full rounded-none border-t">Change Password<div className="flex-1" /><RightArrowIcon width={16} height={16} /></Button>
+        </div>
+        {updatePasswordShow && <UpdatePassword onClose={hideUpdatePassword} />}
+        <Toaster />
       </div>
     );
   }
--- components/update-password-form.tsx
+++ components/update-password-form.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 
-import {useState} from 'react';
+import { useCallback, useState } from 'react';
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
 import { Button } from "./ui/button"
@@ -13,64 +13,92 @@ import { auth } from '../app/firebase'
 import { Toaster } from "@/components/ui/toaster"
 import { toast, useToast } from "@/components/ui/use-toast"
 import { useRouter } from 'next/navigation'
+import { CloseIcon } from "./icons/prompt-ed-icons";
 
-export const UpdatePassword: React.FC = ({}) => {
-    const [isLoading, setIsLoading] = React.useState<boolean>(false);
-    const [password, setPassword] = useState('');
-    const router = useRouter();
+export interface UpdatePasswordProps { className?: string, onClose: () => void};
 
-    const auth = getAuth();
-    async function submitHandler(event: React.SyntheticEvent) {
-        event.preventDefault()
-        setIsLoading(true)
-        const user = auth.currentUser;
-        const newPassword = password;
-    
-        if (user !== null) {
-          await updatePassword(user, newPassword).then(() => {
-            toast({
-              title: "Password Updated",
-            });
-            setIsLoading(false);
-          }).catch((error) => {
-            toast({
-              title: "Account Timeout",
-              description: "Please login then try again"
-            });
-            console.log(error);
-            router.push("/login");
-            setIsLoading(false);
-          });
-        }
-        setTimeout(() => {
-          setIsLoading(false)
-        }, 3000)
-      }
+export const UpdatePassword: React.FC<UpdatePasswordProps> = (props: UpdatePasswordProps) => {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [passwordsMatch, setPasswordsMatch] = useState(false);
+  const router = useRouter();
+
+  const checkPasswordMatch = useCallback((password: string) => {
+    setPasswordsMatch(password == password2);
+  }, [password2]);
+
+  const checkPasswordMatch2 = useCallback((password2: string) => {
+    setPasswordsMatch(password == password2);
+  }, [password]);
+
+  const auth = getAuth();
+  async function submitHandler(event: React.SyntheticEvent) {
+    event.preventDefault()
+    setIsLoading(true)
+    const user = auth.currentUser;
+    const newPassword = password;
+
+    if (user !== null) {
+      await updatePassword(user, newPassword).then(() => {
+        toast({
+          title: "Password Updated",
+        });
+        setIsLoading(false);
+        props.onClose();
+      }).catch((error) => {
+        toast({
+          title: "Account Timeout",
+          description: "Please login then try again"
+        });
+        console.log(error);
+        router.push("/login");
+        setIsLoading(false);
+      });
+    }
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+  }
 
   return (
-    <>
-      <form onSubmit={submitHandler}>
-        <div className="flex max-w-md items-center">
-        <Input
-                id="password"
-                placeholder=""
-                type="password"
-                autoCapitalize="none"
-                autoComplete="off"
-                autoCorrect="off"
-                onChange={(e) => setPassword(e.target.value)}
-                disabled={isLoading}
-                className="mr-5"
-              />
-        <Button disabled={isLoading}>
-              {isLoading && (
-                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-              )}
-              Submit
-        </Button>
-        </div>
-      </form>
-      <Toaster />
-    </>
+    <div className={"absolute w-full h-full flex flex-col items-center pt-[calc(30vh)] bg-[rgba(0,0,0,0.2)] " + props.className}>
+      <div className="grid bg-card-solid p-6 pt-2 rounded-xl">
+        <Button variant="ghost" className="justify-self-end w-2.5 p-0" onClick={props.onClose}><CloseIcon /></Button>
+        <form className="grid gap-1" onSubmit={submitHandler}>
+          <p className="font-normal text-input opacity-60">New Password</p>
+          <Input
+            id="password"
+            type="password"
+            autoCapitalize="none"
+            autoComplete="off"
+            autoCorrect="off"
+            onChange={(e) => { setPassword(e.target.value); checkPasswordMatch(e.target.value); }}
+            disabled={isLoading}
+            className="mr-2"
+            inputSize="lg"
+          />
+          <p className="font-normal text-input opacity-60 mt-2">Confirm New Password</p>
+          <Input
+            id="confirm-password"
+            type="password"
+            autoCapitalize="none"
+            autoComplete="off"
+            autoCorrect="off"
+            onChange={(e) => { setPassword2(e.target.value); checkPasswordMatch2(e.target.value); }}
+            disabled={isLoading}
+            className="mr-2"
+            inputSize="lg"
+          />
+          <p className={"text-[#EB4940] text-input" + (passwordsMatch ? " hidden" : "")}>Passwords don't match.</p>
+          <Button className="mt-1" size="lg" disabled={isLoading || !passwordsMatch}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Change password
+          </Button>
+        </form>
+      </div>
+    </div>
   );
 }
