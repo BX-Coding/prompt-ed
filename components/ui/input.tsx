1b171d5e20257175170e832e7e2e284682a84460
Benjamin Montgomery <benjamin8yrs@gmail.com>
2024-06-28 14:18:31 -0400

    Input variants are different sizes now

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -13,8 +13,8 @@ const inputVariants = cva(
     variants: {
       inputSize: {
         default: "h-input w-full rounded-md",
-        sm: "h-input w-full rounded-md",
-        lg: "h-input w-full rounded-md",
+        sm: "h-5 w-full rounded-md",
+        lg: "h-9 px-4 w-full rounded-md text-base",
         xl: "h-[60px] w-full rounded-lg text-tabs p-[21px]",
       },
     },
